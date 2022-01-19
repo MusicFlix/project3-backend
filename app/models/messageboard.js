763feb7fc508990491fa314e05e7a90932266b36
rob <imafishyfriend@gmail.com>
2022-01-18 21:39:16 -0600

    minor tweaks

--- app/models/messageboard.js
+++ app/models/messageboard.js
@@ -3,23 +3,10 @@ const mongoose = require('mongoose')
 const movieSchema = new mongoose.Schema({
   messages: {
     type: String
-  },
-  link: {
-    type: String,
-    required: true
-  },
-  messageBoard: {
-    type: String
   }
 }, {
-  timestamps: true,
-  toObject: {
-    // remove `hashedPassword` field when we call `.toObject`
-    transform: (_doc, user) => {
-      delete user.hashedPassword
-      return user
-    }
-  }
-})
+  timestamps: true
+}
+)
 
 module.exports = mongoose.model('Movie', movieSchema)
